0bc093d59286757ef2bce59698f9871c50f77c85
William Horn <wbhorn@alaska.edu>
2018-03-30 12:15:06 -0800

    Move color function out of ray class

--- ray.py
+++ ray.py
@@ -1,7 +1,7 @@
 import numpy as np
-from utils import normalized, vec3, Sphere
+from utils import vec3
 
-zeroVec = np.array([0., 0., 0.])
+zeroVec = vec3(0., 0., 0.)
 
 
 class Ray:
@@ -9,24 +9,8 @@ class Ray:
         self.origin = origin
         self.direction = direction
 
-        self.sphere = Sphere(
-            center=vec3(0., 0., -1.),
-            radius=0.5
-        )
-
     def point_at_parameter(time):
         return self.origin + t * self.position
-
-    @property
-    def color(self):
-        if (self.does_hit_sphere(self.sphere)):
-            return vec3(1., 0., 0.)
-
-        unit_dir = normalized(self.direction)[0]
-        t = .5 * (unit_dir[1] + 1.)
-
-        return (1. - t) * np.array([1., 1., 1.]) + \
-            t * np.array([.5, .7, 1.])
 
     def does_hit_sphere(self, s):
         oc = self.origin - s.center
