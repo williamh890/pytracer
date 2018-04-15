3649fc7552e54d36cb98ce635ec88cc200b7b60d
William Horn <wbhorn@alaska.edu>
2018-04-15 12:28:49 -0800

    Multiprocess diffuse object

--- tracer.py
+++ tracer.py
@@ -3,13 +3,15 @@ from utils import normalized, vec3, pixel, Range
 from sphere import RegularSphere
 from hitable_list import HitableList
 from camera import Camera
+from ray import Ray
 
 import numpy as np
 import sys
 import random
+from multiprocessing import Pool
 # random.uniform(0, 1)
 
-WIDTH, HEIGHT, SAMPLES = 200, 100, 40
+WIDTH, HEIGHT, SAMPLES = 200, 100, 20
 INFINITY = sys.float_info.max
 
 
@@ -18,13 +20,29 @@ def color(ray, world):
     hit = world.does_hit(ray, t_range)
 
     if hit is not None:
-        return .5 * (hit.normal + 1.)
+        target = hit.p + hit.normal + random_in_unit_sphere()
+        newRay = Ray(hit.p, target-hit.p)
+
+        return .5 * color(newRay, world)
 
     unit_dir = normalized(ray.direction)
     t = .5 * (unit_dir[1] + 1.)
 
     return (1. - t) * vec3(1., 1., 1.) + \
         t * vec3(.5, .7, 1.)
+
+
+def random_in_unit_sphere():
+    ones = vec3(1., 1., 1.)
+    while True:
+        p = 2 * vec3(
+            random.uniform(-1., 1.),
+            random.uniform(-1., 1.),
+            random.uniform(-1., 1.)
+        ) - ones
+
+        if p.dot(p) >= 1.0:
+            return p
 
 
 def get_pixels():
@@ -53,21 +71,38 @@ def get_pixels():
     )
     world = HitableList([ground, focus])
 
-    for y in range(height):
-        if y % 10 == 0:
-            print(y)
-        for x in range(width):
-            total_samples = vec3(0, 0, 0)
-            for _ in range(SAMPLES):
-                u = (x + random.uniform(0, 1.)) / 200
-                v = (y + random.uniform(0, 1.)) / 100
+    args = [
+        [x, world, camera] for x in range(width)
+    ]
 
-                ray = camera.get_ray(u, v)
-                total_samples += color(ray, world)
+    with Pool(8) as p:
+        rows = p.map_async(trace_column, args).get()
 
-            pixels[x][y] = total_samples / SAMPLES
+    for x in range(WIDTH):
+        pixels[x] = rows[x]
 
     return pixels
+
+
+def trace_column(args):
+    x, world, camera = args
+
+    if x % 20 == 0:
+        print(x)
+
+    col = np.empty([HEIGHT, 3], dtype="float")
+    for y in range(HEIGHT):
+        total_samples = vec3(0, 0, 0)
+        for _ in range(SAMPLES):
+            u = (x + random.uniform(0, 1.)) / 200
+            v = (y + random.uniform(0, 1.)) / 100
+
+            ray = camera.get_ray(u, v)
+            total_samples += color(ray, world)
+
+        col[y] = total_samples / SAMPLES
+
+    return col
 
 
 if __name__ == "__main__":
